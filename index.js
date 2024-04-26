88e5d432b2137e2b620bdbf47812b1e09940ad49
RenanBatistaDosSantos <168221680+RenanBatistaDosSantos@users.noreply.github.com>
2024-04-26 17:21:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
 function minhaFunção() {
-document.getElementById("demo").innerHTML = "Parágrafo alterado.";
-}
+    document.getElementById("demo").innerHTML = "Parágrafo alterado.";
+    }
+</script>
+</html>
